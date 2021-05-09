38b4be8b31eeca57a0a027660e4bcabb07d2e447
cwher <cwher@outlook.com>
2021-05-09 22:19:18 +0800

    perf: apply prefix sum

--- solver.hpp
+++ solver.hpp
@@ -13,23 +13,36 @@ private:
     int mine_number;
     int width, height;
     vector<vector<int>> board;
-    vector<Pos> next_steps; // click these
-    vector<Pos> next_flags; // put flags on these
+    vector<Block> next_steps; // click these
+    vector<Block> next_flags; // put flags on these
 
-    // detect whether there are some positions
+    // if a block has at least one unknown nearby
+    //  then it can be one of the frontier
+    // true frontier also requires board[i][j]<=8
+    vector<vector<bool>> is_frontier;
+
+    // detect whether there are some blocks
     //  whose capacity equals to # of mines nearby
     // their unknown neighbors are safe (if there's any)
     void detectSafe()
     {
+        PrefixSum<int> s;
+        vector<vector<int>> w(height,
+                              vector<int>(width, 0));
         int stat = MINE | FLAG;
         for (int i = 0; i < height; ++i)
             for (int j = 0; j < width; ++j)
-                if (board[i][j] <= 8)
+                w[i][j] = !!(board[i][j] & stat);
+        s.initSum(w);
+
+        for (int i = 0; i < height; ++i)
+            for (int j = 0; j < width; ++j)
+                if (board[i][j] <= 8 && is_frontier[i][j])
                 {
-                    int cnt = 0;
-                    for (int x = max(i - 1, 0); x <= min(i + 1, height - 1); ++x)
-                        for (int y = max(j - 1, 0); y <= min(j + 1, width - 1); ++y)
-                            cnt += !!(board[x][y] & stat);
+                    int cnt = s.getSum(max(i - 1, 0),
+                                       max(j - 1, 0),
+                                       min(i + 1, height - 1),
+                                       min(j + 1, width - 1));
                     if (cnt > board[i][j])
                         printWarning("detectSafe() Overflow on (" +
                                      std::to_string(i) + "," +
@@ -40,35 +53,63 @@ private:
                                 if (board[x][y] == UNKNOWN)
                                     next_steps.emplace_back(make_pair(x, y));
                 }
+        if (!next_steps.empty())
+        {
+            std::sort(next_steps.begin(),
+                      next_steps.end());
+            next_steps.resize(std::unique(next_steps.begin(),
+                                          next_steps.end()) -
+                              next_steps.begin());
+        }
     }
 
-    // detect whether there are some positions
+    // detect whether there are some blocks
     //  whose capacity equals to # of unknown blocks + mines
     // their unknown neighbors must be mines
     void detectUnsafe()
     {
+        PrefixSum<int> s;
+        vector<vector<int>> w(height,
+                              vector<int>(width, 0));
         int stat = MINE | UNKNOWN | FLAG;
         for (int i = 0; i < height; ++i)
             for (int j = 0; j < width; ++j)
-                if (board[i][j] <= 8)
+                w[i][j] = !!(board[i][j] & stat);
+        s.initSum(w);
+
+        for (int i = 0; i < height; ++i)
+            for (int j = 0; j < width; ++j)
+                if (board[i][j] <= 8 && is_frontier[i][j])
                 {
-                    int cnt = 0;
-                    for (int x = max(i - 1, 0); x <= min(i + 1, height - 1); ++x)
-                        for (int y = max(j - 1, 0); y <= min(j + 1, width - 1); ++y)
-                            cnt += !!(board[x][y] & stat);
+                    // int cnt = 0;
+                    // for (int x = max(i - 1, 0); x <= min(i + 1, height - 1); ++x)
+                    //     for (int y = max(j - 1, 0); y <= min(j + 1, width - 1); ++y)
+                    //         cnt += !!(board[x][y] & stat);
+                    int cnt = s.getSum(max(i - 1, 0),
+                                       max(j - 1, 0),
+                                       min(i + 1, height - 1),
+                                       min(j + 1, width - 1));
                     if (cnt == board[i][j])
                         for (int x = max(i - 1, 0); x <= min(i + 1, height - 1); ++x)
                             for (int y = max(j - 1, 0); y <= min(j + 1, width - 1); ++y)
                                 if (board[x][y] == UNKNOWN)
                                     next_flags.emplace_back(make_pair(x, y));
                 }
+        if (!next_flags.empty())
+        {
+            std::sort(next_flags.begin(),
+                      next_flags.end());
+            next_flags.resize(std::unique(next_flags.begin(),
+                                          next_flags.end()) -
+                              next_flags.begin());
+        }
     }
 
     // randomly choose next position
     void randomNext()
     {
         printWarning("Random Step!");
-        vector<Pos> points;
+        vector<Block> points;
         for (int i = 0; i < height; ++i)
             for (int j = 0; j < width; ++j)
                 if (board[i][j] == UNKNOWN)
@@ -94,12 +135,21 @@ public:
         fin >> height >> width >> mine_number;
         board = vector<vector<int>>(height,
                                     vector<int>(width, 0));
+        is_frontier = vector<vector<bool>>(height,
+                                           vector<bool>(width, false));
         is_empty = true;
         for (int i = 0; i < height; ++i)
             for (int j = 0; j < width; ++j)
             {
                 fin >> board[i][j];
-                is_empty &= board[i][j] == UNKNOWN;
+                if (board[i][j] == UNKNOWN)
+                {
+                    for (int x = max(i - 1, 0); x <= min(i + 1, height - 1); ++x)
+                        for (int y = max(j - 1, 0); y <= min(j + 1, width - 1); ++y)
+                            is_frontier[x][y] = true;
+                }
+                else
+                    is_empty = false;
             }
         fin.close();
     }
@@ -139,31 +189,17 @@ public:
             return;
         }
 
-        // detect positions with sufficient existing mines
+        // detect blocks with sufficient existing mines
         // their remaining must be safe
         detectSafe();
         if (!next_steps.empty())
-        {
-            std::sort(next_steps.begin(),
-                      next_steps.end());
-            next_steps.resize(std::unique(next_steps.begin(),
-                                          next_steps.end()) -
-                              next_steps.begin());
             return;
-        }
 
-        // detect positions with sufficient mines + unknown
+        // detect blocks with sufficient mines + unknown
         // their remaining must be unsafe
         detectUnsafe();
         if (!next_flags.empty())
-        {
-            std::sort(next_flags.begin(),
-                      next_flags.end());
-            next_flags.resize(std::unique(next_flags.begin(),
-                                          next_flags.end()) -
-                              next_flags.begin());
             return;
-        }
 
         randomNext();
     }
--- utils.hpp
+++ utils.hpp
@@ -27,4 +27,38 @@ int randomInt(int mn, int mx)
     return dist(gen);
 }
 
+// use prefix sum
+//  to calculate 2D sum in O(1)
+template <class T>
+class PrefixSum
+{
+private:
+    vector<vector<T>> s;
+
+public:
+    // initialize prefix sum
+    void initSum(const vector<vector<T>> w)
+    {
+        int r = w.size();
+        int c = w.front().size();
+        s = vector<vector<T>>(r + 1,
+                              vector<T>(c + 1, 0));
+        for (int i = 1; i <= r; ++i)
+            for (int j = 1; j <= c; ++j)
+                s[i][j] = w[i - 1][j - 1] +
+                          s[i - 1][j] + s[i][j - 1] -
+                          s[i - 1][j - 1];
+    }
+
+    // up left down right
+    // including boundary
+    T getSum(int u, int l,
+             int d, int r)
+    {
+        // actual l=l+1 ...
+        return s[d + 1][r + 1] + s[u][l] -
+               s[d + 1][l] - s[u][r + 1];
+    }
+};
+
 #endif
